24a11383d0e9ea215935dfa2a88c90122da8d94a
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-05-07 10:32:37 +0200

    jquery added

--- js/script-jquery.js
+++ js/script-jquery.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     $('.arrow-btn').click(function() {
+        $(this).toggleClass('transform rotate-180');
         $(this).prev().slideToggle('slow');
     });
+
+    // $('.exp-box').hover(function() {
+    //     $(this).children().last().toggleClass('animate-bounce');
+    // })
 })
