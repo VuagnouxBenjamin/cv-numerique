a54c1ba669028e3e1d974b34cef94c23470edf08
Benjamin VUAGNOUX <vuagnoux.benjamin1@gmail.com>
2021-05-10 11:28:43 +0200

    formation and projects added

--- js/script-jquery.js
+++ js/script-jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.arrow-btn').click(function() {
-        $(this).toggleClass('transform rotate-180');
+        $(this).toggleClass('transform rotate-180 animate-bounce');
         $(this).prev().slideToggle('slow');
     });
 
